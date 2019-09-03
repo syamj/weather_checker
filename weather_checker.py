6e0d94caf8b1d21c6d2252b5cd9c610873263905
Syam J Prakash <syaaam616@gmail.com>
2019-09-03 23:23:21 +0530

    Create weather_checker.py

---
+++ weather_checker.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+
+api_key = "68a4a9c2eef038410098a962f513749c"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+city_name = input("Enter your city name: ")
+
+full_url = base_url + "q=" + city_name + "&APPID=" + api_key
+
+response = requests.get(full_url)
+x = response.json()
+
+
+if x['cod'] == '404':
+    print("City not found!!!!")
+else:
+    print("****************Weather report of {},{} *************".format(x['name'], x['sys']['country']) )
+    max_temp = x['main']['temp_max'] - 273.15
+    min_temp = x['main']['temp_min'] - 273.15
+    for i in x['weather']:
+        forecast = (i['description'])
+    print("Forecast : {}".format(forecast))
+    print("Maximum Temperature : {}°C".format(max_temp))
+    print("Minimum Temperature : {}°C".format(min_temp))
+    print("Humidity : {}".format(x['main']['humidity']))
+    
